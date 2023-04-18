f6aa6e72bb44019c6fcea0450d1c052c7308d6e2
Slevin-Block <slevin@SlevinBlock>
2023-04-18 23:25:55 +0200

    [style] fix relative position header v5

--- components/FourthCover/FourthCover.tsx
+++ components/FourthCover/FourthCover.tsx
@@ -5,12 +5,10 @@ export default function FourthCover() {
   return (
     <div className={styles.book} >
         <h3 className={styles.title} >DE MATHIEU PETOT</h3>
-        <div className={styles.body} >
-            <p>Noé est un vieil ermite bougon et misanthrope. Quand le Déluge s’abat sur la Terre, il échappe de peu à la catastrophe avec sa famille et tous les animaux.</p>
-            <p>À bord de l’arche, pourtant, rien ne se passe comme prévu : les poutres se démontent toutes seules, des animaux disparaissent de manière inexplicable tandis que les fils de Noé réclament désespérément la terre ferme…</p>
-            <p>Mais quand les eaux se retireront, les hommes sauront-ils habiter le nouveau monde sans colère ni gourmandise ? Comment l’équilibre subtil pourra-t-il être trouvé ?</p>
-            <p>À mi-chemin entre le conte et la fable écologique, ce récit réécrit de façon loufoque et poétique la plus vieille histoire du monde. Le tout surprend par son étonnante actualité.</p>
-        </div>
+        <p>Noé est un vieil ermite bougon et misanthrope. Quand le Déluge s’abat sur la Terre, il échappe de peu à la catastrophe avec sa famille et tous les animaux.</p>
+        <p>À bord de l’arche, pourtant, rien ne se passe comme prévu : les poutres se démontent toutes seules, des animaux disparaissent de manière inexplicable tandis que les fils de Noé réclament désespérément la terre ferme…</p>
+        <p>Mais quand les eaux se retireront, les hommes sauront-ils habiter le nouveau monde sans colère ni gourmandise ? Comment l’équilibre subtil pourra-t-il être trouvé ?</p>
+        <p>À mi-chemin entre le conte et la fable écologique, ce récit réécrit de façon loufoque et poétique la plus vieille histoire du monde. Le tout surprend par son étonnante actualité.</p>
     </div>
   )
 }
